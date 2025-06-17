7fba31d95ccaa1695962cd8cb25ad0d1bc50cf71
Tony Gaskell <tony@sudokrew.com>
2025-06-16 19:46:51 -1000

    Update grid containers for confrimation pages

--- src/pages/OnlineConfirmation.jsx
+++ src/pages/OnlineConfirmation.jsx
@@ -1,18 +1,23 @@
+
 import "../index.css";
 import React from "react";
-import { Button } from "@trussworks/react-uswds";
+import { Button, GridContainer, Grid } from "@trussworks/react-uswds";
 import { useNavigate } from "react-router-dom";
 
 function OnlineConfirmation() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="display-flex flex-column flex-align-center padding-y-4 padding-x-2 text-center">
-        <div className="width-full maxw-mobile-lg">
-          <h1>Success!</h1>
-          <p>Your trip has been successfully submitted!</p>
-        </div>
-      </div>
+      <GridContainer className="padding-y-4 padding-x-0 width-full maxw-mobile-lg">
+        <Grid row>
+          <Grid col="fill">
+            <div className="text-center">
+              <h1>Success!</h1>
+              <p>Your trip has been successfully submitted!</p>
+            </div>
+          </Grid>
+        </Grid>
+      </GridContainer>
 
       <footer className="position-fixed bottom-0 width-full bg-gray-5 padding-bottom-2 padding-x-2 shadow-1 z-top">
         <div className="display-flex flex-justify maxw-mobile-lg margin-x-auto padding-top-2">
